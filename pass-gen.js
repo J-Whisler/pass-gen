bfc6c77cb815214f9984fcd1bb2950df930d607f
Jacob Whisler <jwhisler521@gmail.com>
2019-11-17 13:08:51 -0500

    added for loop

--- pass-gen.js
+++ pass-gen.js
@@ -10,3 +10,7 @@ var lowerCharset = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 var upperCarset = ['A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '>', '<', '{', '}', '[', ']','-','_', '`','~',]
 var numChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+for (i = 0; i < passLength; i++) {
+    finalPass1[i] = (lowerCharset[Math.floor(Math.random() * 26)])
+}

c565d962653af6c6278c5ae00bfbe305a2e1b030
Jacob Whisler <jwhisler521@gmail.com>
2019-11-22 18:12:10 -0500

    added functions to all 4 character sets to produce random

--- pass-gen.js
+++ pass-gen.js
@@ -1,26 +1,69 @@
-var finalPass= ""
 
-var passLength = "";
+var passLength = 8
 
-var lowerCharset = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var upperCarset = ['A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '>', '<', '{', '}', '[', ']','-','_', '`','~',]
-var numChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-var charNum = document.getElementById('charNum')
+function lower () {
+    var lowerCharset = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    return lowerCharset[Math.floor(Math.random) * lowerCharset.length]
+}
 
-var uppercase = document.getElementById('uppercase')
+function upper () {
+    var upperCharset = ['A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    return upperCarset[Math.floor(Math.random) * upperCharset.length]
+}
 
-var lowercase = document.getElementById('lowercase')
+function specials () {
+    var spec = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '>', '<', '{', '}', '[', ']','-','_', '`','~',]
+    return spec[Math.floor(Math.random) * spec.length]
+}
 
-var number = document.getElementById('number')
+function numbers () {
+    var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    return num[Math.floor(Math.random) * num.length]
+}
 
-var symbol = document.getElementById('symbol')
 
-var generate = document.getElementById('generatePwd')
+var arrPass = []
+var stringPass = ''
+var finalPass = ''
 
-var copy = document.getElementById('copyToClipboard')
+finalPass2 = (finalPass.toString())
+finalPass3 = (finalPass2.replace(/,/g,""))
 
-var password = document.getElementById('display')
+function generate (){
+ // var passLength = document.getElementById('charNum').value;
+ passLength = 8       
+ 
+ for (var i = 0; i < passLength; i++) {
+  finalPass[i] = (lowerCharset[Math.floor(Math.random() * lowerCharset.length)])
+}
+
+
+document.getElementById('display').value = finalPass3;
+}
+
+function convert() {
+    finalPass2 = (finalPass.toString())
+    finalPass3 = (finalPass2.replace(/,/g,""))
+}
+
+
+function run () {
+    generate()
+    convert()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

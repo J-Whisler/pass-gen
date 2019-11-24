4bf079ef724170c6757af6d379e2a9ee61b23dfd
Jacob Whisler <jwhisler521@gmail.com>
2019-11-23 19:53:28 -0500

    fixed possible outcomes functions, updated styling

--- pass-gen.js
+++ pass-gen.js
@@ -1,25 +1,26 @@
-
-var passLength = 8
-
+var lowerCharset = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upperCharset = ['A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var spec = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '>', '<', '{', '}', '[', ']','-','_', '`','~']
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 function lower () {
-    var lowerCharset = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    return lowerCharset[Math.floor(Math.random) * lowerCharset.length]
+    return lowerCharset
+    //[Math.floor(Math.random() * lowerCharset.length)]
 }
 
 function upper () {
-    var upperCharset = ['A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    return upperCarset[Math.floor(Math.random) * upperCharset.length]
+    return upperCharset
+    //[Math.floor(Math.random() * upperCharset.length)]
 }
 
 function specials () {
-    var spec = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '>', '<', '{', '}', '[', ']','-','_', '`','~',]
-    return spec[Math.floor(Math.random) * spec.length]
+    return spec
+    //[Math.floor(Math.random() * spec.length)]
 }
 
 function numbers () {
-    var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    return num[Math.floor(Math.random) * num.length]
+    return num
+    //[Math.floor(Math.random() * num.length)]
 }
 
 
@@ -27,8 +28,7 @@ var arrPass = []
 var stringPass = ''
 var finalPass = ''
 
-stringPass = (arrPass.toString())
-finalPass = (stringPass.replace(/,/g,""))
+var passLength = 8
 
 var low = true
 var up = true
@@ -36,7 +36,7 @@ var specs = true
 var nums = true
 
 function userInput () {
-    passLength = document.getElementById("charNum").value
+    passLength = document.querySelector("#charNum").value
 
 
     if (document.getElementById("uppercase").checked) {
@@ -67,19 +67,69 @@ function userInput () {
 }
 
 
-document.getElementById('display').value = finalPass3;
+function passSanitize () {
+    if (passLength < 8 || passLength > 128) {
+        alert("Please choose a number bewtween 8 and 128 characters for password length")
+
+    } else if (low === false && up === false && specs === false && nums === false) {
+        alert("Please check at least one of the boxes to choose at least one of the character sets")
+   
 }
+
+arrPass.length = 0
+}
+
+function createPassword () {
+
+    var possibleValues = []
+
+    if (low === true) {
+        possibleValues = possibleValues.concat(lower())
+    }
+
+    if (up === true) {
+        possibleValues = possibleValues.concat(upper())
+    }
+
+    if (specs === true) {
+        possibleValues = possibleValues.concat(specials())
+    }
+    if (nums === true) {
+        possibleValues = possibleValues.concat(numbers())
+    }
+
+
+    for (var i = 0; i < passLength; i++) {
+        //console.log(possibleValues)
+        var randomize = function(){
+            return Math.floor(Math.random() * possibleValues.length)
+        }
+            arrPass.push(possibleValues[randomize()])
+        
+    }
+}
+
 
 function convert() {
-    finalPass2 = (finalPass.toString())
-    finalPass3 = (finalPass2.replace(/,/g,""))
+    stringPass = (arrPass.toString())
+    finalPass = (stringPass.replace(/,/g,""))
+}
+
+function final () {
+     document.getElementById("display").value = finalPass
+    
 }
 
 
-function run () {
-    generate()
-    convert()
+function generate () {
+    userInput();
+    passSanitize();
+    createPassword();
+    convert();
+    final();
 }
+
+
 
 
 
